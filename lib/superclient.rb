1153d8233936ac712d11f168911759b0099bbe81
Sylvance <kerandisylvance@gmail.com>
2023-01-18 02:52:12 +0300

    [superclient] Bundle initial gem files

---
+++ lib/superclient.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "superclient/version"
+
+module Superclient
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/superclient_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Superclient do
+  it "has a version number" do
+    expect(Superclient::VERSION).not_to be nil
+  end
+end
